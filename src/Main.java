8fcc5a132b43c3678e5b0864cf7327db9feaddcf
worleystidham <stidhawd@mail.uc.edu>
2024-06-03 21:47:01 -0400

    take 1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // let's do this;
         double subTotal = 0.00;
-        double taxRate = 0.05;
+        double TAX_RATE = 0.05;
         double salesTaxAm = 0.00;
         double taxedTotal = 0.00;
 
         System.out.println("Please enter the price of your item.");
         subTotal = 59.99;
-        salesTaxAm = subTotal * taxRate;
+        salesTaxAm = subTotal * TAX_RATE;
         taxedTotal = subTotal + salesTaxAm;
-        System.out.println("Your subtotal is " + subTotal + ".  Taxed at a rate of " + taxRate + ", you w" +
+        System.out.println("Your subtotal is " + subTotal + ".  Taxed at a rate of " + TAX_RATE + ", you w" +
                 "ill pay " + salesTaxAm + " in tax for a total of " + taxedTotal + ".");
 
 
